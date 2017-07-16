1e9f7ffe6e0cefd7692f7b8dbaf592b2b9438c80
Dovydas Bartkevicius <dovydas@default.lt>
2017-07-16 16:33:17 +0100

    Fixed the hn client

--- hn/client.go
+++ hn/client.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"sync"
 )
 
 const (
@@ -37,7 +36,6 @@ type baseClient struct {
 }
 
 func (c *baseClient) TopStories() ([]int, error) {
-	return []int{14779881, 14780709, 14778977, 14778685, 14780159, 14779509, 14778335}, nil
 	rsp, err := c.h.Get(topStoriesAPI)
 
 	if rsp != nil {
@@ -61,51 +59,7 @@ func (c *baseClient) TopStories() ([]int, error) {
 	return stories, nil
 }
 
-var cache = map[int]*Story{
-	14779881: &Story{
-		ID:    14779881,
-		Score: 571,
-		Title: "Apache Foundation bans projects from using React's “BSD+Patent” Code",
-	},
-	14780709: &Story{
-		ID:    14780709,
-		Score: 35,
-		Title: "The Deal on the Table (1994)",
-	},
-	14778977: &Story{
-		ID:    14778977,
-		Score: 177,
-		Title: "A 32-year-old state senator is trying to get patent trolls out of Mass (techcrunch.com)",
-	},
-	14778685: &Story{
-		ID:    14778685,
-		Score: 279,
-		Title: "Monolith First (2015)",
-	},
-	14780159: &Story{
-		ID:    14780159,
-		Score: 71,
-		Title: "A deep dive into Multicore OCaml garbage collector",
-	},
-	14779509: &Story{
-		ID:    14779509,
-		Score: 101,
-		Title: "Why MAC address randomization is not enough [pdf]",
-	},
-	14778335: &Story{
-		ID:    14778335,
-		Score: 207,
-		Title: "Tokyo street fashion and culture: 1980 – 2017 ",
-	},
-}
-
-var cm sync.Mutex
-
 func (c *baseClient) Story(id int) (*Story, error) {
-	cm.Lock()
-	defer cm.Unlock()
-
-	return cache[id], nil
 	rsp, err := c.h.Get(fmt.Sprintf(itemAPI, id))
 
 	if rsp != nil {
